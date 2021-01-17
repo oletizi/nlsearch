59fdcc74cbdcdaab7f50c3caa9ba04badcc3565e
Orion Letizi <oletizi@mac.com>
2021-01-16 17:16:03 -0800

    - changed backupdb.sh user to root so it will backup all databases

--- bin/backupdb.sh
+++ bin/backupdb.sh
@@ -3,4 +3,4 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 outfile=${dir}/../sql/init/dump.sql
 
-mysqldump --protocol=tcp -h localhost -u bds -p --all-databases > "${outfile}"
+mysqldump --protocol=tcp -h localhost -u root -p --all-databases > "${outfile}"
--- sql/init/aaa-init.sql
+++
@@ -1,8 +0,0 @@
-create database bds;
-
-CREATE USER 'bds'@'localhost' IDENTIFIED BY 'bds';
-CREATE USER 'bds'@'%' IDENTIFIED BY 'bds';
-
-GRANT ALL ON *.* TO 'bds'@'localhost';
-GRANT ALL ON *.* TO 'bds'@'%';
-flush privileges;
